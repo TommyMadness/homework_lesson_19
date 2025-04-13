2429e6088c3e48e4d7516ea4da2947e1f08cf0e0
atansan <tommymadness@gmail.com>
2025-04-13 14:09:38 +0300

    homework for lesson 19

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,5 +1,5 @@
 import pytest
-from allure_commons._allure import step
+import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from locators import (
     search_button_locators,
@@ -9,33 +9,41 @@ from locators import (
 from selene import browser, have, be
 
 
+@allure.title("Search for 'Selenium' and verify results appear")
 def test_check_search_functionality(mobile_management):
-    with step("Type search"):
+    with allure.step("Skip iOS app if not Wikipedia"):
         platform = mobile_management
         if platform == "ios":
             pytest.skip("iOS app is not Wikipedia, skipping test.")
+
+    with allure.step("Click on the search button"):
         browser.element(search_button_locators[platform]).click()
+
+    with allure.step("Type 'Selenium' into search input"):
         browser.element(search_input_locators[platform]).type("Selenium")
         results = browser.all(search_result_locators[platform])
 
-    with step("Verify content found"):
+    with allure.step("Check that search results are present and correct"):
         results.should(have.size_greater_than(0))
         results.first.should(have.text("Selenium"))
 
 
+@allure.title("Search for 'BrowserStack' and open article")
 def test_check_that_search_result_can_be_opened(mobile_management):
-    with step("Type search"):
+    with allure.step("Skip iOS app if not Wikipedia"):
         platform = mobile_management
         if platform == "ios":
             pytest.skip("iOS app is not Wikipedia, skipping test.")
+
+    with allure.step("Search for 'BrowserStack'"):
         browser.element(search_button_locators[platform]).click()
         browser.element(search_input_locators[platform]).type("BrowserStack")
         results = browser.all(search_result_locators[platform])
 
-    with step("Open article"):
+    with allure.step("Click on the first result"):
         results.first.click()
 
-    with step("Verify article opened"):
+    with allure.step("Verify article is opened"):
         browser.element(
             (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("BrowserStack")')
         ).should(be.visible)
